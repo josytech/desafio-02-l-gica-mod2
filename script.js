952d4217db51f206e6965e041d502463a0f91111
josytech <josytech4@gmail.com>
2022-01-24 22:33:16 -0300

    desafio 02 lógica mod 02

---
+++ script.js
@@ -0,0 +1,41 @@
+// 1-Crie uma condição que exiba uma mensagem no console se a idade for maior que 18
+
+let idade = 31;
+
+if(idade >= 18){
+ console.log("Você é maior de idade,parabéns!")
+} else{
+  console.log("Você não é maior de idade, sorry!")
+}
+
+
+//2 - Crie uma condição que exiba uma mensagem no console se você for maior de idade E a condição humana seja true
+
+let humano=true
+if(idade>= 18 && humano==true){
+    console.log("Você é um ser humano adulto,aproveite!")
+}
+
+
+
+
+   // 3- Criar uma condição que exiba uma mensagem no console se você fizer aniversário em Janeiro OU Dezembro
+   let mesDeNiver="setembro"
+   if(mesDeNiver=="janeiro" || mesDeNiver=="dezembro"){
+       console.log("faço aniversario na primavera")
+   }
+
+   //*** Exercícios bônus
+//4 - Criar uma condição que exiba uma mensagem no console se seu nome começar com a letra R
+
+let Inicial="J"
+if(Inicial=="R"){
+    console.log("meu nome começa com a letra J")
+} else{
+    console.log("meu nome não começa com R")
+}
+
+
+
+
+
